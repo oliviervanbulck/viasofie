ab2cea484e7b06539daa265b8ff92eeccc9141a5
oliviervanbulck <olivier@vanbulck.com>
2016-04-19 13:53:29 +0200

    Edit Panden

--- ViaSofie/panden/models.py
+++ ViaSofie/panden/models.py
@@ -6,4 +6,14 @@ from django.db import models
 # Create your models here.
 class Pand(models.Model):
     prijs = models.FloatField()
-    test = models.FloatField()
+    bouwjaar = models.DateField()
+    oppervlakte = models.FloatField()
+    algemene_beschrijving = models.CharField(1000)
+
+
+class Type(models.Model):
+    type = models.CharField(255)
+
+
+class Foto(models.Model):
+    foto_link = models.CharField()
