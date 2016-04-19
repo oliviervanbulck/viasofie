66917dd9e3fd05b7dbaac5e37170cecff27a8ed7
oliviervanbulck <olivier@vanbulck.com>
2016-04-19 14:50:55 +0200

    Panden keys

--- ViaSofie/panden/models.py
+++ ViaSofie/panden/models.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from gebruikers.models import Gebruiker
+from gebruikers.models import Adres
 
 
 # Create your models here.
@@ -9,6 +11,9 @@ class Pand(models.Model):
     bouwjaar = models.DateField()
     oppervlakte = models.FloatField()
     algemene_beschrijving = models.TextField()
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    gebruiker = models.ForeignKey(Gebruiker, on_delete=models.CASCADE)
+    adres = models.ForeignKey(Adres, on_delete=models.CASCADE)
 
 
 class Type(models.Model):
@@ -17,10 +22,15 @@ class Type(models.Model):
 
 class Foto(models.Model):
     foto_link = models.CharField(max_length=255)
+    pand = models.ForeignKey(Pand, on_delete=models.CASCADE)
 
 
 class PandKenmerkPerPand(models.Model):
+    class Meta:
+        unique_together = (('pand', 'kenmerk'),)
     aantal = models.PositiveSmallIntegerField()
+    pand = models.ForeignKey(Pand, on_delete=models.CASCADE)
+    kenmerk = models.ForeignKey(Kenmerk, on_delete=models.CASCADE)
 
 
 class Kenmerk(models.Model):
@@ -30,3 +40,4 @@ class Kenmerk(models.Model):
 class PandImmoLink(models.Model):
     site_beschrijving = models.CharField(max_length=255)
     site_link = models.CharField(max_length=255)
+    pand = models.ForeignKey(Pand, on_delete=models.CASCADE)
