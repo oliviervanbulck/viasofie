d4dbbf92417305c28725e5292372d4f9f09ba01d
Tim <timcocx788@gmail.com>
2016-04-19 13:50:37 +0200

    Added the Gebruikers and Dossiers apps

--- ViaSofie/gebruikers/models.py
+++ ViaSofie/gebruikers/models.py
@@ -2,8 +2,4 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-
 # Create your models here.
-class Pand(models.Model):
-    prijs = models.FloatField()
-    test = models.FloatField()
