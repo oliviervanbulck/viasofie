c45c9a27a779942f304d5cb4a2bc802748ccf5a5
oliviervanbulck <olivier@vanbulck.com>
2016-04-21 23:16:40 +0200

    Adding urls en views pages + testing page with master layout include and static files

--- ViaSofie/panden/views.py
+++ ViaSofie/panden/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.template import loader
+
 
 # Create your views here.
+def index(request):
+    test = "Hello world!"
+    context = {
+        'test': test
+    }
+    return render(request, 'panden/index.html', context)
