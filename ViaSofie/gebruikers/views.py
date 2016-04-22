641142f7ff0e9d48b12de9e7211c15875beabc17
oliviervanbulck <olivier@vanbulck.com>
2016-04-22 13:39:09 +0200

    Added template examples & authentication form

--- ViaSofie/gebruikers/views.py
+++ ViaSofie/gebruikers/views.py
@@ -1,4 +1,33 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import loader
+from django.template import RequestContext
+from django.contrib.auth import authenticate, login, logout
+
 
 # Create your views here.
+def index(request):
+    if request.POST:
+        return login_user(request)
+    context = {
+    }
+    return render(request, 'gebruikers/login.html', context)
+
+
+def login_user(request):
+    username = password = ''
+    if request.POST:
+        #logout(request)
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/gebruiker/')
+    return render(request, 'gebruikers/login.html', context=RequestContext(request))
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/gebruiker/')
