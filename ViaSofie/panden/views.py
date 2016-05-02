9ec8bfe8f8e9b453f4af0a9b914d6324724a072e
Glenn Michiels <glenn.programmeur@gmail.com>
2016-05-02 15:21:54 +0200

    CSS Panden search bar + images

--- ViaSofie/panden/views.py
+++ ViaSofie/panden/views.py
@@ -6,7 +6,7 @@ from ViaSofie.functions import get_random_actieve_panden
 # Create your views here.
 def index(request):
     test = "Hello world!"
-    panden = get_random_actieve_panden(9)
+    panden = get_random_actieve_panden(1)
     context = {
         'test': test,
         'nbar': 'panden',
