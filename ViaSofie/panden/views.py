748d49e771a2f6ab59e83144ee58b015ef39f8a3
Glenn Michiels <glenn.programmeur@gmail.com>
2016-05-02 14:42:41 +0200

    CSS: Panden

--- ViaSofie/panden/views.py
+++ ViaSofie/panden/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.template import loader
+from ViaSofie.functions import get_random_actieve_panden
 
 
 # Create your views here.
 def index(request):
     test = "Hello world!"
+    panden = get_random_actieve_panden(9)
     context = {
         'test': test,
-        'nbar': 'panden'
+        'nbar': 'panden',
+        'panden': panden
     }
     return render(request, 'panden/index.html', context)
