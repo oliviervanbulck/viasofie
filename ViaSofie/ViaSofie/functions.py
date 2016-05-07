363790799175d44ac9a6461db434920bd3637192
Tim <timcocx788@gmail.com>
2016-05-07 13:03:30 +0200

    Made the House Types & Towns load dynamically

--- ViaSofie/ViaSofie/functions.py
+++ ViaSofie/ViaSofie/functions.py
@@ -1,7 +1,17 @@
 from panden.models import Pand
+from gebruikers.models import Adres
 import random
 
 
 def get_random_actieve_panden(aantal):
     panden = Pand.objects.filter(dossier__actief=True)
     return random.sample(panden, min(aantal, len(panden)))
+
+
+# Kan mogelijk efficienter
+def get_alle_gemeentes():
+    tmp = Adres.objects.values('gemeente').distinct()
+    returnable = []
+    for gemeente in tmp:
+        returnable.append(gemeente['gemeente'])
+    return returnable
