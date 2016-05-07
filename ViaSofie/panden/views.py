d5b8b56e430249ca0342a41ad5229cd7db31de47
Tim <timcocx788@gmail.com>
2016-05-07 13:03:30 +0200

    Made the House Types & Towns load dynamically

--- ViaSofie/panden/views.py
+++ ViaSofie/panden/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from django.template import loader
 from ViaSofie.functions import get_random_actieve_panden
+from ViaSofie.functions import get_alle_gemeentes
 from .models import Pand
+from .models import Type
 
 
 # Create your views here.
 def index(request):
     test = "Hello world!"
     panden = get_random_actieve_panden(1)
+    pand_types = Type.objects.all()
+    gemeentes = get_alle_gemeentes()
+
     context = {
         'test': test,
         'nbar': 'panden',
-        'panden': panden
+        'panden': panden,
+        'pand_types': pand_types,
+        'gemeentes': gemeentes,
     }
     return render(request, 'panden/index.html', context)
 
