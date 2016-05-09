12191f359ccf001506bad92cfab4e1e4fe0ffc7e
michaelvanderborght <michael.vanderborght.mv@gmail.com>
2016-05-09 13:59:38 +0200

    Changed contact to django form

---
+++ ViaSofie/ViaSofie/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    email = forms.CharField(label='Email address', max_length=100,widget=forms.TextInput(attrs={'class' : 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class' : 'form-control', 'size': 150}))
