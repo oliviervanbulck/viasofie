24554109ffdb89835600c13df537c1ce9bec231d
oliviervanbulck <olivier@vanbulck.com>
2016-05-13 11:24:59 +0200

    Implemented keyword search

--- ViaSofie/panden/views.py
+++ ViaSofie/panden/views.py
@@ -1,27 +1,33 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from ViaSofie.functions import get_alle_actieve_panden
 from ViaSofie.functions import get_alle_gemeentes
+from ViaSofie.functions import keyword_search
 from .models import Pand
 from .models import Type
 
 
 # Create your views here.
 def index(request):
-    test = "Hello world!"
     pand_types = Type.objects.all()
     gemeentes = get_alle_gemeentes()
 
-    panden = get_alle_actieve_panden()
-    pand_kolom_class = 'col-lg-4 col-md-6'
-
     context = {
-        'test': test,
         'nbar': 'kopen',
-        'panden': panden,
         'pand_types': pand_types,
         'gemeentes': gemeentes,
-        'pand_kolom_class': pand_kolom_class,
     }
+
+    if request.POST and request.POST['search'] and ('clearPanden' not in request.POST):
+        panden = keyword_search(Pand, request.POST['search'],
+                                ('adres__straat', 'adres__gemeente', 'adres__postcode', 'adres__huisnummer',
+                                 'type__type',))
+        context['search'] = request.POST['search']
+    else:
+        panden = get_alle_actieve_panden()
+    context['pand_kolom_class'] = 'col-lg-4 col-md-6'
+    context['panden'] = panden
+
     return render(request, 'panden/index.html', context)
 
 
