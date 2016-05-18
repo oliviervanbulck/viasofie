f88f9013fb29b5059c1c0a49989ffc4cf6bea95f
Octamir <timcocx788@gmail.com>
2016-05-18 18:08:49 +0200

    Removed duplicate code for kopen/huren

--- ViaSofie/panden/views.py
+++ ViaSofie/panden/views.py
@@ -8,13 +8,12 @@ from .models import Type
 from .forms import AdvancedSearchForm
 
 
-# Create your views here.
-def index(request):
+def panden_general(request, nbar_val):
     pand_types = Type.objects.all()
     gemeentes = get_alle_gemeentes()
 
     context = {
-        'nbar': 'kopen',
+        'nbar': nbar_val,
         'pand_types': pand_types,
         'gemeentes': gemeentes,
         'form': AdvancedSearchForm()
@@ -31,30 +30,15 @@ def index(request):
     context['panden'] = panden
 
     return render(request, 'panden/index.html', context)
+
+
+# Create your views here.
+def index(request):
+    return panden_general(request, 'kopen')
 
 
 def huren(request):
-    pand_types = Type.objects.all()
-    gemeentes = get_alle_gemeentes()
-
-    context = {
-        'nbar': 'huren',
-        'pand_types': pand_types,
-        'gemeentes': gemeentes,
-        'form': AdvancedSearchForm()
-    }
-
-    if request.POST and request.POST['search'] and ('clearPanden' not in request.POST):
-        panden = keyword_search(Pand, request.POST['search'],
-                                ('adres__straat', 'adres__gemeente', 'adres__postcode', 'adres__huisnummer',
-                                 'type__type',))
-        context['search'] = request.POST['search']
-    else:
-        panden = get_alle_actieve_panden()
-    context['pand_kolom_class'] = 'col-lg-4 col-md-6'
-    context['panden'] = panden
-
-    return render(request, 'panden/index.html', context)
+    return panden_general(request, 'huren')
 
 
 def pand_detail(request, pand_id):
