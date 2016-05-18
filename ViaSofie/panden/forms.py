41cbe157d5fb92611739be0b2451cd2aa8029255
Octamir <timcocx788@gmail.com>
2016-05-18 18:01:45 +0200

    Edited the Advanced Search form

--- ViaSofie/panden/forms.py
+++ ViaSofie/panden/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from ViaSofie.functions import get_alle_gemeentes
+from panden.models import Type
 
 
 class NameForm(forms.Form):
@@ -7,11 +8,11 @@ class NameForm(forms.Form):
               ('nee', 'Nee'),
               ('eender', 'Eender')]
 
-    # De gemeentes moeten in een bepaald formaat!
-    GEMEENTES = []
-    for gemeente in get_alle_gemeentes():
-        GEMEENTES.append((gemeente, gemeente))
+    # De gemeentes & soorten moeten in een bepaald formaat!
+    GEMEENTES = [(gemeente, gemeente) for gemeente in get_alle_gemeentes()]
+    SOORTEN = [(soort, soort) for soort in Type.objects.all()]
 
-    gemeentes = forms.ChoiceField(choices=GEMEENTES)
-    zwembad = forms.ChoiceField(choices=KEUZES, widget=forms.RadioSelect())
-    tuin = forms.ChoiceField(choices=KEUZES, widget=forms.RadioSelect())
+    gemeente = forms.ChoiceField(choices=GEMEENTES, label='Gemeentes', required=True)
+    soort = forms.ChoiceField(choices=SOORTEN, label='Soorten', required=True)
+    zwembad = forms.ChoiceField(choices=KEUZES, widget=forms.RadioSelect(), label='Zwembad', required=True, initial='eender')
+    tuin = forms.ChoiceField(choices=KEUZES, widget=forms.RadioSelect(), label='Tuin', required=True, initial='eender')
