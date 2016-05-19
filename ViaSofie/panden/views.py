3bc3e7c0e7f2ef7a3b24539d9904719f2ab0d3cd
octamir <timcocx788@gmail.com>
2016-05-19 15:53:41 +0200

    Made the advanced search code a little more optimized

--- ViaSofie/panden/views.py
+++ ViaSofie/panden/views.py
@@ -32,48 +32,67 @@ def panden_general(request, nbar_val):
                                  'type__type',))
         context['search'] = request.POST['search']
     elif check_get_parameters():
+        def filter_prijs(panden):
+            return [pand for pand in panden if int(request.GET['prijs_lower']) <= pand.prijs <= int(request.GET['prijs_upper'])]
+        def filter_slaapkamers(panden):
+            return [pand for pand in panden if pand.pandkenmerkperpand_set.filter(kenmerk__benaming='Aantal slaapkamers')
+                    and (int(request.GET['slaapkamer_lower']) <= pand.pandkenmerkperpand_set.filter(kenmerk__benaming='Aantal slaapkamers')[0].aantal <= int(request.GET['slaapkamer_upper']))]
+        def filter_gemeente(panden):
+            if request.GET['gemeente'] != '':
+                return [pand for pand in panden if pand.adres.gemeente == request.GET['gemeente']]
+            return panden
+        def filter_soort(panden):
+            if request.GET['soort'] != '':
+                return [pand for pand in panden if pand.type.type == request.GET['soort']]
+            return panden
+        def filter_zwembad(panden):
+            zwembad = request.GET['zwembad']
+            ZWEMBAD_KENMERK = 'Zwembad'
+            if zwembad != 'eender':
+                if zwembad == 'ja':
+                    return [pand for pand in panden if
+                              pand.pandkenmerkperpand_set.filter(kenmerk__benaming=ZWEMBAD_KENMERK)
+                              and pand.pandkenmerkperpand_set.filter(kenmerk__benaming=ZWEMBAD_KENMERK)[0].aantal == 1]
+                else:
+                    return [pand for pand in panden if
+                              not pand.pandkenmerkperpand_set.filter(kenmerk__benaming=ZWEMBAD_KENMERK)
+                              or (pand.pandkenmerkperpand_set.filter(kenmerk__benaming=ZWEMBAD_KENMERK)
+                                  and pand.pandkenmerkperpand_set.filter(kenmerk__benaming=ZWEMBAD_KENMERK)[0].aantal == 0)]
+            return panden
+        def filter_tuin(panden):
+            tuin = request.GET['tuin']
+            TUIN_KENMERK = 'Oppervlakte tuin'
+            if tuin != 'eender':
+                if tuin == 'ja':
+                    return [pand for pand in panden if
+                              pand.pandkenmerkperpand_set.filter(kenmerk__benaming=TUIN_KENMERK)
+                              and pand.pandkenmerkperpand_set.filter(kenmerk__benaming=TUIN_KENMERK)[0].aantal > 0]
+                else:
+                    return [pand for pand in panden if
+                              not pand.pandkenmerkperpand_set.filter(kenmerk__benaming=TUIN_KENMERK)
+                              or (pand.pandkenmerkperpand_set.filter(kenmerk__benaming=TUIN_KENMERK)
+                                  and pand.pandkenmerkperpand_set.filter(kenmerk__benaming=TUIN_KENMERK)[
+                                      0].aantal == 0)]
+            return panden
+
+
         # Make sure the form doesn't reset
         context['form'] = AdvancedSearchForm(request.GET)
 
         panden = get_alle_actieve_panden()
+        filters = [filter_prijs,
+                   filter_slaapkamers,
+                   filter_gemeente,
+                   filter_soort,
+                   filter_tuin,
+                   filter_zwembad]
 
-        # Prijs filter
-        panden = [pand for pand in panden if int(request.GET['prijs_lower']) <= pand.prijs <= int(request.GET['prijs_upper'])]
-
-        # Slaapkamers filter
-        panden = [pand for pand in panden if pand.pandkenmerkperpand_set.filter(kenmerk__benaming='Aantal slaapkamers')
-                  and (int(request.GET['slaapkamer_lower']) <= pand.pandkenmerkperpand_set.filter(kenmerk__benaming='Aantal slaapkamers')[0].aantal <= int(request.GET['slaapkamer_upper']))]
-
-        # Gemeente filter
-        if request.GET['gemeente'] != '':
-            panden = [pand for pand in panden if pand.adres.gemeente == request.GET['gemeente']]
-
-        # Soort filter
-        if request.GET['soort'] != '':
-            panden = [pand for pand in panden if pand.type.type == request.GET['soort']]
-
-        # Zwembad filter
-        zwembad = request.GET['zwembad']
-        ZWEMBAD_KENMERK = 'Zwembad'
-        if zwembad != 'eender':
-            if zwembad == 'ja':
-                panden = [pand for pand in panden if pand.pandkenmerkperpand_set.filter(kenmerk__benaming=ZWEMBAD_KENMERK)
-                          and pand.pandkenmerkperpand_set.filter(kenmerk__benaming=ZWEMBAD_KENMERK)[0].aantal == 1]
+        # Elke filter maakt de gevonden panden specifieker en specifieker, maar als we met een lege lijst zitten moet er niet verder gefilterd worden (break)
+        for filter in filters:
+            if panden:
+                panden = filter(panden)
             else:
-                panden = [pand for pand in panden if not pand.pandkenmerkperpand_set.filter(kenmerk__benaming=ZWEMBAD_KENMERK)
-                          or (pand.pandkenmerkperpand_set.filter(kenmerk__benaming=ZWEMBAD_KENMERK)
-                              and pand.pandkenmerkperpand_set.filter(kenmerk__benaming=ZWEMBAD_KENMERK)[0].aantal == 0)]
-
-        tuin = request.GET['tuin']
-        TUIN_KENMERK = 'Oppervlakte tuin'
-        if tuin != 'eender':
-            if tuin == 'ja':
-                panden = [pand for pand in panden if pand.pandkenmerkperpand_set.filter(kenmerk__benaming=TUIN_KENMERK)
-                          and pand.pandkenmerkperpand_set.filter(kenmerk__benaming=TUIN_KENMERK)[0].aantal > 0]
-            else:
-                panden = [pand for pand in panden if not pand.pandkenmerkperpand_set.filter(kenmerk__benaming=TUIN_KENMERK)
-                          or (pand.pandkenmerkperpand_set.filter(kenmerk__benaming=TUIN_KENMERK)
-                              and pand.pandkenmerkperpand_set.filter(kenmerk__benaming=TUIN_KENMERK)[0].aantal == 0)]
+                break
     else:
         panden = get_alle_actieve_panden()
     context['pand_kolom_class'] = 'col-lg-4 col-md-6'
