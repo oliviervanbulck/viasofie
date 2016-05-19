4011d7f9ee7cec195746b7d5376034ac6881c725
michaelvanderborght <michael.vanderborght.mv@gmail.com>
2016-05-19 15:49:59 +0200

    Added dossier index

--- ViaSofie/dossiers/urls.py
+++ ViaSofie/dossiers/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='dossiers.index'),
+    url(r'dossiertest$', views.dossier, name='dossiers.dossiertest'),
+    url(r'(?P<dossier_id>[0-9]+)$', views.testje, name='idk testje'),
 ]
