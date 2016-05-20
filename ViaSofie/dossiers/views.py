98e07b9273ff2d36d5f00155ad07ac759c5397fc
michaelvanderborght <michael.vanderborght.mv@gmail.com>
2016-05-20 14:30:16 +0200

    Added dossier pand detail & docs

--- ViaSofie/dossiers/views.py
+++ ViaSofie/dossiers/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
+from dossiers.models import Dossier
+from panden.models import Pand
+
 
 def index(request):
     panden = request.user.gebruiker.pand_set.all()
@@ -11,12 +14,18 @@ def index(request):
     return render(request, "Dossier/index.html", context)
 
 
-def dossier(request):
-
+def dossier(request, dossier_id):
+    dossier_obj = Dossier.objects.get(id=dossier_id)
+    pand = Pand.objects.get(id=dossier_obj.pand_id)
+    doclijnen = dossier_obj.dossierdoclijn_set.all()
     context = {
-        'dossier_id'
+        'dossier':dossier_obj,
+        'basis_kenmerken': [(pand.adres, 'Adres'),(pand.prijs, 'Prijs'),(pand.type, 'Type'),(pand.bouwjaar, 'Bouwjaar'),(pand.oppervlakte, 'Oppervlakte')],
+        'kenmerken':pand.pandkenmerkperpand_set.all().order_by('kenmerk__benaming'),
+        'foto':pand.foto_set.first(),
+        'doclijnen':doclijnen,
     }
-    return render(request, "Dossier/dossier.html")
+    return render(request, "Dossier/dossier.html",context)
 
-def testje(request, dossier_id):
-    return HttpResponse('ayyyyy' + dossier_id)
+#def testje(request, dossier_id):
+    #return HttpResponse('ayyyyy' + dossier_id)
