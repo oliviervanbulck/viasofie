59ec228e320683db90123ec8d3c0230c54d86f35
oliviervanbulck <olivier@vanbulck.com>
2016-05-24 15:06:01 +0200

    -

--- ViaSofie/ViaSofie/admin.py
+++ ViaSofie/ViaSofie/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from django.contrib.sites.models import Site
 
-admin.autodiscover() # Zonder autodiscover vindt Django het geregistreerde model niet!
-admin.site.unregister(Site) # Dit model moet niet in het admin panel verschijnen
+admin.autodiscover()  # Zonder autodiscover vindt Django het geregistreerde model niet!
+admin.site.unregister(Site)  # Dit model moet niet in het admin panel verschijnen
