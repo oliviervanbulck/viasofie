ec32d271841ab38415f166140fa0811b2c121a57
Olivier Van Bulck <olivier@vanbulck.com>
2016-05-25 16:52:10 +0200

    Added autocomplete for Adres

--- ViaSofie/gebruikers/views.py
+++ ViaSofie/gebruikers/views.py
@@ -8,6 +8,7 @@ from django.db.models import Value as V
 
 
 # Create your views here.
+from gebruikers.models import Woonplaats, Land
 from panden.models import Type
 
 
@@ -38,3 +39,36 @@ def logout_user(request):
     logout(request)
     return HttpResponseRedirect('/')
 
+
+class WoonplaatsAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        # Don't forget to filter out results depending on the visitor !
+        if not self.request.user.is_authenticated():
+            return Woonplaats.objects.none()
+
+        if not self.request.user.is_superuser:
+            return Woonplaats.objects.none()
+
+        qs = Woonplaats.objects.all()
+
+        if self.q:
+            qs = qs.annotate(volledig=Concat('postcode', V(' '), 'gemeente')).filter(volledig__icontains=self.q)
+
+        return qs
+
+
+class LandAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        # Don't forget to filter out results depending on the visitor !
+        if not self.request.user.is_authenticated():
+            return Land.objects.none()
+
+        if not self.request.user.is_superuser:
+            return Land.objects.none()
+
+        qs = Land.objects.all()
+
+        if self.q:
+            qs = qs.annotate(volledig=Concat('landcode', V(' '), 'naam')).filter(volledig__icontains=self.q)
+
+        return qs
