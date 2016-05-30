67a0ea57a2d71e95a2d84606af2e4c3270b28294
Olivier Van Bulck <olivier@vanbulck.com>
2016-05-30 15:23:03 +0200

    Filters euro en oppervlakte

--- ViaSofie/ViaSofie/templatetags/viasofie_filters.py
+++ ViaSofie/ViaSofie/templatetags/viasofie_filters.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 from django import template
 from django.contrib.auth.models import Group
+from django.contrib.humanize.templatetags.humanize import intcomma
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
@@ -8,3 +11,13 @@ register = template.Library()
 def has_group(user, group_name):
     group = Group.objects.get(name=group_name)
     return True if group in user.groups.all() else False
+
+
+@register.filter(name='euro')
+def in_euro(waarde):
+    return mark_safe("&euro; %s" % (intcomma(int(waarde))))
+
+
+@register.filter(name='opp')
+def in_opp(waarde):
+    return "%s m²" % waarde
