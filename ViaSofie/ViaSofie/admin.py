df8a809874d27bc7e01c95c5b106abde1f107aba
Olivier Van Bulck <olivier@vanbulck.com>
2016-06-03 01:50:43 +0200

    Adding menu bar admin

--- ViaSofie/ViaSofie/admin.py
+++ ViaSofie/ViaSofie/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 
 admin.autodiscover()  # Zonder autodiscover vindt Django het geregistreerde model niet!
 admin.site.unregister(Site)  # Dit model moet niet in het admin panel verschijnen
+admin.site.unregister(Group)
