16a8f7746fcfa7de1295b859951e43bf0b8b0dc4
octamir <timcocx788@gmail.com>
2016-06-06 09:40:26 +0200

    Optimized the price tags in advanced search

--- ViaSofie/ViaSofie/static/ViaSofie/js/main.js
+++ ViaSofie/ViaSofie/static/ViaSofie/js/main.js
@@ -2,18 +2,6 @@
  * Created by Tim on 17/05/2016.
  */
 
-// Source: http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
-Number.prototype.formatMoney = function (c, d, t) {
-    var n = this,
-        c = isNaN(c = Math.abs(c)) ? 2 : c,
-        d = d == undefined ? "." : d,
-        t = t == undefined ? "," : t,
-        s = n < 0 ? "-" : "",
-        i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
-        j = (j = i.length) > 3 ? j % 3 : 0;
-    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-};
-
 function makeSlider(elementId, lowerId, upperId, min, max, step, lowerFormattedId, upperFormattedId) {
     // Optionele parameters!
     lowerFormattedId = lowerFormattedId || null;
@@ -46,12 +34,14 @@ function makeSlider(elementId, lowerId, upperId, min, max, step, lowerFormattedI
         if (!handle) {
             lowerValue.value = tmp;
             if (lowerFormatted != null) {
-                lowerFormatted.innerHTML = tmp.formatMoney(0, '.', ',')
+                //lowerFormatted.innerHTML = tmp.formatMoney(0, '.', ',')
+                lowerFormatted.innerHTML = tmp.toLocaleString();
             }
         } else {
             upperValue.value = tmp;
             if (upperFormatted != null) {
-                upperFormatted.innerHTML = tmp.formatMoney(0, '.', ',')
+                //upperFormatted.innerHTML = tmp.formatMoney(0, '.', ',')
+                upperFormatted.innerHTML = tmp.toLocaleString();
             }
         }
     });
