ad1ee19aca6788bf4121421a15c507f93ece6a8c
octamir <timcocx788@gmail.com>
2016-06-06 15:40:45 +0200

    Changed a pretty important typo!

--- ViaSofie/ViaSofie/models.py
+++ ViaSofie/ViaSofie/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-class faq_item(models.Model):
+class FaqItem(models.Model):
     class Meta:
         verbose_name = 'FAQ item'
     titel = models.CharField(max_length=255)
