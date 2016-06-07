912033139d369a4eda8ae886ebc900ef60550677
Tim <timcocx788@gmail.com>
2016-06-07 10:59:17 +0200

    Removed a now redundant import

--- ViaSofie/ViaSofie/admin.py
+++ ViaSofie/ViaSofie/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
-from django import forms
 from .models import FaqItem, Partner
 
 
