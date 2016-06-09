96e4d0f63412e7a7ec82b7d0bfed93d954d6a816
Tim <timcocx788@gmail.com>
2016-06-09 21:06:00 +0200

    Reordered the middleware like said on StackOverflow

--- ViaSofie/ViaSofie/settings.py
+++ ViaSofie/ViaSofie/settings.py
@@ -51,13 +51,13 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'ViaSofie.urls'
