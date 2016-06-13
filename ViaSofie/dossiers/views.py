97291d70f7eeb072bd45da0a9537305e2c2b4690
octamir <timcocx788@gmail.com>
2016-06-13 09:46:56 +0200

    Added login_required

--- ViaSofie/dossiers/views.py
+++ ViaSofie/dossiers/views.py
@@ -1,13 +1,14 @@
 from django.core.mail import EmailMessage
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from ViaSofie.templatetags.viasofie_filters import in_euro, in_opp
 
 from dossiers.forms import ContactFormDossier
 from panden.models import Pand
 
 
+@login_required()
 def index(request):
     panden = request.user.gebruiker.pand_set.all()
     context = {
@@ -18,51 +19,48 @@ def index(request):
     return render(request, "Dossier/index.html", context)
 
 
+@login_required()
 def dossier(request, pand_id):
-    if request.user.is_authenticated():
-        pand = Pand.objects.get(id=pand_id)
-        if request.user.id == pand.gebruiker.user.id:
-            doclijnen = pand.dossierdoclijn_set.all()
-            stavaza = pand.stavazalijn_set.all()
+    pand = Pand.objects.get(id=pand_id)
+    if request.user.id == pand.gebruiker.user.id:
+        doclijnen = pand.dossierdoclijn_set.all()
+        stavaza = pand.stavazalijn_set.all()
 
-            context = {
-                'pand': pand,
-                'ref_nummer': pand.ref_number(),
-                'basis_kenmerken': [(pand.adres, 'Adres'), (in_euro(pand.prijs), 'Prijs'), (pand.type, 'Type'),
-                                    (pand.bouwjaar, 'Bouwjaar'), (in_opp(pand.oppervlakte), 'Oppervlakte'),
-                                    (pand.algemene_beschrijving, 'Beschrijving')],
-                'kenmerken': pand.pandkenmerkperpand_set.all().order_by('kenmerk__benaming'),
-                'foto': pand.foto_set.first(),
-                'doclijnen': doclijnen,
-                'stavazalijnen': stavaza,
-                'form': ContactFormDossier(),
-                'prev_url': request.META.get('HTTP_REFERER', None),
-            }
-            if request.method == 'GET':
-                context.update({'nbar': 'dossier'})
-                return render(request, "Dossier/dossier.html", context)
+        context = {
+            'pand': pand,
+            'ref_nummer': pand.ref_number(),
+            'basis_kenmerken': [(pand.adres, 'Adres'), (in_euro(pand.prijs), 'Prijs'), (pand.type, 'Type'),
+                                (pand.bouwjaar, 'Bouwjaar'), (in_opp(pand.oppervlakte), 'Oppervlakte'),
+                                (pand.algemene_beschrijving, 'Beschrijving')],
+            'kenmerken': pand.pandkenmerkperpand_set.all().order_by('kenmerk__benaming'),
+            'foto': pand.foto_set.first(),
+            'doclijnen': doclijnen,
+            'stavazalijnen': stavaza,
+            'form': ContactFormDossier(),
+            'prev_url': request.META.get('HTTP_REFERER', None),
+        }
+        if request.method == 'GET':
+            context.update({'nbar': 'dossier'})
+            return render(request, "Dossier/dossier.html", context)
 
-            elif request.method == 'POST':
-                form = ContactFormDossier(request.POST, request.FILES)
+        elif request.method == 'POST':
+            form = ContactFormDossier(request.POST, request.FILES)
 
-                try:
-                    if form.is_valid():
-                        email = request.user.email
-                        message = form.cleaned_data['message']
-                        attachment = request.FILES['attachment']
-                        email = EmailMessage('', email + '\n\n' + message, to=['michael.vanderborght.mv@gmail.com'])
-                        if attachment:
-                            email.attach(attachment.name, attachment.read(), attachment.content_type)
-                        email.send()
-                        context.update({'succes': True, 'form': ContactFormDossier()})
-                        return render(request, context, 'Dossier/dossier.html',context)
-                    else:
-                        raise Exception()
-                except:
-                    context.update({'error': True, 'form': form})
-                    return render(request, 'Dossier/dossier.html', context)
-        else:
-            return redirect('dossiers.index')
+            try:
+                if form.is_valid():
+                    email = request.user.email
+                    message = form.cleaned_data['message']
+                    attachment = request.FILES['attachment']
+                    email = EmailMessage('', email + '\n\n' + message, to=['michael.vanderborght.mv@gmail.com'])
+                    if attachment:
+                        email.attach(attachment.name, attachment.read(), attachment.content_type)
+                    email.send()
+                    context.update({'succes': True, 'form': ContactFormDossier()})
+                    return render(request, context, 'Dossier/dossier.html',context)
+                else:
+                    raise Exception()
+            except:
+                context.update({'error': True, 'form': form})
+                return render(request, 'Dossier/dossier.html', context)
     else:
-        # Redirect the user to the login if he's not logged in
-        return redirect(settings.LOGIN_URL)
+        return redirect('dossiers.index')
