219dce147f3af91a4de4310ddc9da0c5cbb82857
Olivier Van Bulck <olivier@vanbulck.com>
2016-06-21 11:38:59 +0200

    Removed url of profile of user

--- ViaSofie/gebruikers/urls.py
+++ ViaSofie/gebruikers/urls.py
@@ -4,7 +4,6 @@ from gebruikers.views import WoonplaatsAutocomplete, LandAutocomplete
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='gebruikers.index'),
     url(r'^login/$', views.login_user, name='gebruikers.login'),
     url(r'^logout/$', views.logout_user, name='gebruikers.logout'),
     url(
